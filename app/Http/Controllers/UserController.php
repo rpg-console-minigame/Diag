9f687bfa7061f4f88bcf637dbc862573e00fc085
aqp0001 <aqp0001@alu.medac.es>
2025-01-28 20:11:19 +0100

    avance

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,16 +14,33 @@ class UserController extends Controller
 {
     public function Datos()
     {
-        if(session('user')-> is_admin){
+        if (session('user')->is_admin) {
             $sedes = Sede::all();
             return view('register', ['sedes' => $sedes]);
-        }
-        else {
+        } else {
             return redirect()->route('welcome');
         }
     }
 
-    public function Guardar(){
+    public function mostrarUsuarios()
+    {
+        if(session('user')->is_admin){
+            $users = User::where('id' , session('user')->id)->get();
+            // add allusers to $users
+            $users ->merge(User::where('id', '!=', session('user')->id)->get());
+
+            foreach ($users as $user) {
+                $user->sede = Sede::where('id', $user->sede_id)->first();
+            }
+
+            return view('editarUsuarios', ['users' => $users]);
+        }else{
+            return redirect()->route('welcome');
+        }
+    }
+
+    public function Guardar()
+    {
         $data = request()->all();
         $user = new User();
         $user->name = $data['name'];
@@ -44,27 +61,38 @@ class UserController extends Controller
         session_start();
         $data = request()->all();
         $user = User::where('email', $data['email'])->first();
-        if($user && password_verify($data['password'], $user->password)){
+        if ($user && password_verify($data['password'], $user->password)) {
             session(['user' => $user]);
             return redirect()->route('welcome');
         }
-        return redirect()->route('login');  
+        return redirect()->route('login');
     }
     public function welcomeWittData()
     {
         session_start();
-        if(!session('user')){
+        if (!session('user')) {
             return redirect()->route('login');
-        }
-        else {
-            $muestras = Muestra::where('user_id', session('user')->getKey())->get();
-            foreach ($muestras as $muestra) {
-                $muestra->formato = Formato_muestra::where('id', $muestra->formato_muestra_id)->first();
-                $muestra->sede = Sede::where('id', $muestra->sede_id)->first();
-                $muestra->tipo_naturaleza = Tipo_naturaleza::where('id', $muestra->tipo_naturaleza_id)->first();
-                $muestra->calidad = Calidad::where('id', $muestra->calidad_id)->first();
+        } else {
+
+            if (session('user')->is_admin) {
+                $muestras = Muestra::all();
+                foreach ($muestras as $muestra) {
+                    $muestra->formato = Formato_muestra::where('id', $muestra->formato_muestra_id)->first();
+                    $muestra->sede = Sede::where('id', $muestra->sede_id)->first();
+                    $muestra->tipo_naturaleza = Tipo_naturaleza::where('id', $muestra->tipo_naturaleza_id)->first();
+                    $muestra->calidad = Calidad::where('id', $muestra->calidad_id)->first();
+                }
+                return view('welcomeAdmin', ['muestras' => $muestras]);
+            } else {
+                $muestras = Muestra::where('user_id', session('user')->getKey())->get();
+                foreach ($muestras as $muestra) {
+                    $muestra->formato = Formato_muestra::where('id', $muestra->formato_muestra_id)->first();
+                    $muestra->sede = Sede::where('id', $muestra->sede_id)->first();
+                    $muestra->tipo_naturaleza = Tipo_naturaleza::where('id', $muestra->tipo_naturaleza_id)->first();
+                    $muestra->calidad = Calidad::where('id', $muestra->calidad_id)->first();
+                }
+                return view('welcome', ['muestras' => $muestras]);
             }
-            return view('welcome', ['muestras' => $muestras]);
         }
     }
     public function logout()
