ac40b2172e3c36bf729519bbd1084bc761f1a1c4
aqp0001 <aqp0001@alu.medac.es>
2025-02-05 20:07:57 +0100

    validator

--- app/Http/Controllers/InterpretacionController.php
+++ app/Http/Controllers/InterpretacionController.php
@@ -8,6 +8,7 @@ use App\Models\Interpretacion_texto;
 use App\Models\Interpretacion;
 use App\Models\Muestra;
 use App\Models\Tipo_estudio;
+use Illuminate\Support\Facades\Validator;
 
 class InterpretacionController extends Controller
 {
@@ -22,11 +23,19 @@ class InterpretacionController extends Controller
     }
     public function create(Request $request)
     {
+        Validator::make($request->all(), [
+            'texto' => 'required|min:20|max:255',
+            'id_muestra' => 'required|exists:id_muestra',
+            'id_interpretacion' => 'required|exists:id_interpretacion'
+        ]);
+
         $interpretacionTexto = new Interpretacion_texto();
         $interpretacionTexto->texto = $request->texto;
         $interpretacionTexto->id_muestra = $request->id_muestra;
         $interpretacionTexto->id_interpretacion = $request->id_interpretacion;
         $interpretacionTexto->save();
-        return redirect()->route('muestra', ['id' => $request->id_muestra]);
+
+        return redirect()->route('muestra', ['id' => $request->id_muestra])
+        ->with('success', 'Interpretacion guardada con exito');
     }
 }
