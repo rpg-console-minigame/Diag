276878686b7b41ca4b5f014f4c3f7a2c853b2809
aqp0001 <aqp0001@alu.medac.es>
2025-02-06 20:43:02 +0100

    Validator_interpretaciones

--- app/Http/Controllers/InterpretacionController.php
+++ app/Http/Controllers/InterpretacionController.php
@@ -23,19 +23,35 @@ class InterpretacionController extends Controller
     }
     public function create(Request $request)
     {
-        Validator::make($request->all(), [
+        $validator = Validator::make($request->all(), 
+        [
             'texto' => 'required|min:20|max:255',
-            'id_muestra' => 'required|exists:id_muestra',
-            'id_interpretacion' => 'required|exists:id_interpretacion'
+            'id_muestra' => 'exists:muestra,id',
+            'id_interpretacion' => 'exists:interpretacion,id'
+        ],
+        [
+            'texto.required' => 'El texto es requerido',
+            'texto.min' => 'El texto debe tener al menos 20 caracteres',
+            'texto.max' => 'El texto debe tener maximo 255 caracteres',
+            'id_muestra.exists' => 'La muestra no existe',
+            'id_interpretacion.exists' => 'La interpretacion no existe'
         ]);
 
-        $interpretacionTexto = new Interpretacion_texto();
-        $interpretacionTexto->texto = $request->texto;
-        $interpretacionTexto->id_muestra = $request->id_muestra;
-        $interpretacionTexto->id_interpretacion = $request->id_interpretacion;
-        $interpretacionTexto->save();
+        if($validator->fails())
+        {
+            return redirect()->route('muestra', ['id' => $request->id_muestra])
+            ->withErrors($validator)
+            ->withInput();
+        }
+        else{
+            $interpretacionTexto = new Interpretacion_texto();
+            $interpretacionTexto->texto = $request->texto;
+            $interpretacionTexto->id_muestra = $request->id_muestra;
+            $interpretacionTexto->id_interpretacion = $request->id_interpretacion;
+            $interpretacionTexto->save();
 
-        return redirect()->route('muestra', ['id' => $request->id_muestra])
-        ->with('success', 'Interpretacion guardada con exito');
+            return redirect()->route('muestra', ['id' => $request->id_muestra])
+            ->with('success', 'Interpretacion guardada con exito');
+        }
     }
 }
